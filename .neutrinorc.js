c59108f995b9a93ab104967be7f39b4bcfcb9430
helfi92 <helfi92@gmail.com>
2017-06-27 15:14:46 -0700

    Migrate to v6

---
+++ .neutrinorc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  use: [
+    'neutrino-preset-react',
+    neutrino => neutrino.config
+      .entry('vendor')
+      .add('react')
+      .add('react-dom')
+      .add('react-hot-loader')
+  ]
+};
